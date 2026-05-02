37d040cd29f4cd6a07f600a67e96650b278a000d
Kamano8 <amadoukamano73@gmail.com>
2026-05-02 16:13:54 +0000

    Fix superuser avec nom et prenom requis

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(email='admin@naissancechain.gn').exists():
-    User.objects.create_superuser(email='admin@naissancechain.gn', password='Admin2026@')
+    User.objects.create_superuser(email='admin@naissancechain.gn', password='Admin2026@', nom='Admin', prenom='Super')
     print('Superuser cree!')
 else:
     print('Superuser existe deja.')
