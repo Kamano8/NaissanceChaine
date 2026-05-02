b7d2e9ed43275767d6d6bb891b451cc18f6c52a4
Kamano8 <amadoukamano73@gmail.com>
2026-05-02 15:53:54 +0000

    Fix chemin script superuser

--- scripts/create_superuser.py
+++ scripts/create_superuser.py
@@ -1,9 +1,14 @@
 import os
+import sys
 import django
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
+
 from django.contrib.auth import get_user_model
 User = get_user_model()
+
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser(username='admin', email='admin@naissancechain.gn', password='Admin2026@')
     print("Superuser cree avec succes !")
