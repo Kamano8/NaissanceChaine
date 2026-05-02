1678a909a15bad5ee5013f879815dd1779a8d90f
Kamano8 <amadoukamano73@gmail.com>
2026-05-02 16:04:49 +0000

    Superuser via shell Django directement

--- build.sh
+++ build.sh
@@ -3,4 +3,12 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python scripts/create_superuser.py
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(email='admin@naissancechain.gn').exists():
+    User.objects.create_superuser(email='admin@naissancechain.gn', password='Admin2026@')
+    print('Superuser cree!')
+else:
+    print('Superuser existe deja.')
+"
